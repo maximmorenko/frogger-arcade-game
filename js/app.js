8dc344d43d70d399f195f77dc609a63578aaf5e2
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-04 19:09:06 +0300

    класс позиционирования, враги и игрок

--- js/app.js
+++ js/app.js
@@ -1,39 +1,147 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const countRows = 6;
+const countColumns = 5;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+const tileHight = 80;
+const tileWidth = 105;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+class Position {
+    startX;
+    startY;
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    x;
+    y;
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    minY;
+    minX;
+    maxY;
+    maxX;
+    
+    constructor( row, col ) {
+        // at the entrance we are waiting for the coordinates of the tiles of all members (player, enemies)
+        this.startX = (tileWidth * col) - (tileWidth);
+        this.startY = (tileHight * row) - (tileHight / 2); 
+        this.x = this.startX;
+        this.y = this.startY;
+
+        this.maxY = tileHight * (countRows - 1);
+        this.maxX = tileWidth * (countColumns - 1);
+        this.minY = tileHight * (row - 1) - this.startY;
+        this.minX = 0;
+    }
+
+    reset() {
+        this.x = this.startX;
+        this.y = this.startY;
+        
+    }
+}
+class PositionEnemy extends Position {
+
+    run(speed, dt) {
+        this.x += (speed * dt); 
+    }
+}
+
+class PositionPlayer extends Position {
+
+    moveUp() {
+        let newLocY = this.y - cellHeight;
+        if (newLocY >= this.minY) {
+            this.y = newLocY; 
+        }
+    }
+
+    moveDown() {
+        let newLocY = this.y + cellHeight;
+        if (newLocY <= this.maxY) {
+            this.y = newLocY; 
+        }
+    }
+
+    moveRigtht() {
+        let newLocX = this.x + cellWidth;
+        if (newLocX <= this.maxX) {
+            this.x = newLocX; 
+        }
+    }
+
+    moveLeft() {
+        let newLocX = this.x - cellWidth;
+        if (newLocX >= this.minX) {
+            this.x = newLocX; 
+        }
+    }
+}
+
+class Enemy {
+
+    speed = (Math.floor(Math.random(4) * (500 - 100 + 1)) + 100);
+    sprite = 'images/enemy-bug.png';
+
+    constructor ( PositionEnemy ) {
+        this.position = PositionEnemy;
+    }
+
+    update (dt) {
+        if (this.position.x <= this.position.maxX) {
+            this.position.run(this.speed, dt);
+        } else {
+            this.position.reset();
+        }
+    }
+
+    render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.position.x, this.position.y);
+    }
+}
+
+class Player {
+    sprite = 'images/char-horn-girl.png';
+
+    constructor ( PositionPlayer ) {
+        this.position = PositionPlayer;
+    }
+
+    alertWin() {
+        if (this.position.y == this.position.minY) {
+            setTimeout(() => {
+                alert("Not bed! Try again!");
+                this.position.reset();
+            }, 0);
+        }
+    }
+
+    update () {
+        this.alertWin();
+    }
+
+    handleInput (keyCode) {
+
+        switch(keyCode) {
+            case 'up': 
+                this.position.moveUp();
+                break;
+            case 'down': 
+                this.position.moveDown();
+                break;
+            case 'right': 
+                this.position.moveRigtht();
+                break;
+            case 'left': 
+                this.position.moveLeft();
+                break;
+            default: 
+                break;
+        }
+    }
+
+    render () {
+        ctx.drawImage(Resources.get(this.sprite), this.position.x, this.position.y);
+    }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
